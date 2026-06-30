02a44b0e0d35393f5cb9a60eb4ad3eac88aea289
Ryanghyeon <ksc749873@gmail.com>
2026-07-01 03:46:11 +0900

    Add files via upload

---
+++ api/auth/discord/discord.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  const clientId = process.env.DISCORD_CLIENT_ID;
+  const redirectUri = process.env.DISCORD_REDIRECT_URI;
+
+  const url =
+    `https://discord.com/oauth2/authorize` +
+    `?client_id=${clientId}` +
+    `&response_type=code` +
+    `&scope=identify%20email` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}`;
+
+  res.writeHead(302, {
+    Location: url,
+  });
+
+  res.end();
+}

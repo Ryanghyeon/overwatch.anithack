36feb950802c2644b89d1be701899368454c0ef6
ininin0423 <ininin0423@gmail.com>
2026-07-05 01:02:44 +0900

    Update firebaseadmin.js

--- api/auth/discord/firebaseadmin.js
+++ api/auth/discord/firebaseadmin.js
@@ -1,8 +1,7 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+  const serviceAccount = JSON.parse(process.env.firebase_discord);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
   });

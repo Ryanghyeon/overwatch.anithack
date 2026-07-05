f89a4033d569c548c508f298005276abd5c5672a
Ryanghyeon <ksc749873@gmail.com>
2026-07-05 21:31:49 +0900

    Simplify Firebase Admin setup and export
    Refactor Firebase Admin initialization and export.

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,25 +1,15 @@
+import pkg from "firebase-admin";
 
-let admin;
+const admin = pkg.default || pkg;
 
-export async function getAdmin() {
-  if (!admin) {
-    const firebaseAdmin = await import("firebase-admin");
-
-    // ✅ 각각 분리
-    const app = firebaseAdmin.default || firebaseAdmin;
-
-    admin = app;
-
-    if (!admin.apps || admin.apps.length === 0) {
-      admin.initializeApp({
-        credential: app.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-        }),
-      });
-    }
-  }
-
-  return admin;
+if (!admin.apps || admin.apps.length === 0) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
 }
+
+export default admin;

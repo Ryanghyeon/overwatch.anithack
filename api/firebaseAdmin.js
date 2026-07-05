d0821a6a849c863cafbba386a9479817d028b7c3
Ryanghyeon <ksc749873@gmail.com>
2026-07-05 21:15:11 +0900

    Refactor firebaseAdmin to separate app initialization

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -5,12 +5,14 @@ export async function getAdmin() {
   if (!admin) {
     const firebaseAdmin = await import("firebase-admin");
 
-    // ✅ 핵심 (이 줄)
-    admin = firebaseAdmin.default || firebaseAdmin;
+    // ✅ 각각 분리
+    const app = firebaseAdmin.default || firebaseAdmin;
+
+    admin = app;
 
     if (!admin.apps || admin.apps.length === 0) {
       admin.initializeApp({
-        credential: admin.credential.cert({
+        credential: app.credential.cert({
           projectId: process.env.FIREBASE_PROJECT_ID,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),

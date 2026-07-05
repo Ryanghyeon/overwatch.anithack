26fa0d8d8a864653e8edb9529cc1ef81aeca6c71
Ryanghyeon <ksc749873@gmail.com>
2026-07-05 21:12:15 +0900

    Update firebaseAdmin.js

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,27 +1,22 @@
 
-import admin from "firebase-admin";
+let admin;
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-  });
+export async function getAdmin() {
+  if (!admin) {
+    const firebaseAdmin = await import("firebase-admin");
+
+    admin = firebaseAdmin.default;
+
+    if (!admin.apps.length) {
+      admin.initializeApp({
+        credential: admin.credential.cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+        }),
+      });
+    }
+  }
+
+  return admin;
 }
-
-export default admin;
-
-const serviceAccount = JSON.parse(process.env.firebase_discord);
-
-const app =
-  getApps().length === 0
-    ? initializeApp({
-        credential: cert(serviceAccount),
-      })
-    : getApps()[0];
-
-const auth = getAuth(app);
-
-export default auth;
